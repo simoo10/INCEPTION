bcc7d0bb2abf835fddd76dab165790956d0d7250
MOHAMED ET-TAHERY <simotahiri010@gmail.com>
2024-02-22 17:28:44 +0100

    INCEPTION

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,9 +3,9 @@
 mysql_install_db;
 service mysql start;
 
-mysql -e "create database if not exists mydata;"
-mysql -e "create user if not exists user1 identified by 'user1234';"
-mysql -e "GRANT ALL PRIVILEGES ON mydata.* TO user1;"
+mysql -e "create database if not exists $DBNAME;"
+mysql -e "create user if not exists $DBUSER identified by '$DBPASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO $DBUSER;"
 mysql -e "FLUSH PRIVILEGES;"
 service mariadb stop
 mysql
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -2,8 +2,8 @@
 
 sleep 10
 wp core download --allow-root
-wp config create --dbhost=mariadb --dbname=mydata --dbuser=user1 --dbpass=user1234 --allow-root
-wp core install --url=https://met-tahe.42.fr --title=website --admin_user=met-tahe --admin_password=met-tahe123 --admin_email=met-tahe@gmail.com --allow-root
-wp user create usr usr@gmail.com --role=subscriber --user_pass=usr123@ --allow-root
+wp config create --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --allow-root
+wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINEMAIL --allow-root
+wp user create $USER $USEREMAIL --role=subscriber --user_pass=$USERPASS --allow-root
 sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 php-fpm7.3 -F -R --nodaemonize
