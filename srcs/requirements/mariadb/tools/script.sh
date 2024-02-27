be8c43202eb4dadaabba33ac8f8c3f99c6083ddc
MOHAMED ET-TAHERY <simotahiri010@gmail.com>
2024-02-27 20:04:18 +0100

    Inception

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,8 +4,8 @@ mysql_install_db;
 service mysql start;
 
 mysql -e "create database if not exists $DBNAME;"
-mysql -e "create user if not exists $DBUSER identified by '$DBPASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO $DBUSER;"
+mysql -e "create user if not exists '$DBUSER' identified by '$DBPASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER';"
 mysql -e "FLUSH PRIVILEGES;"
-service mariadb stop
-mysql
+service mysql stop
+exec mysqld_safe
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -4,6 +4,6 @@ sleep 10
 wp core download --allow-root
 wp config create --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --allow-root
 wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINEMAIL --allow-root
-wp user create $USER $USEREMAIL --role=subscriber --user_pass=$USERPASS --allow-root
+wp user create $USER $USEREMAIL --role=subscriber --user_pass=USERPASS --allow-root
 sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 php-fpm7.3 -F -R --nodaemonize
